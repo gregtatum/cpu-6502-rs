36a0fac75f48519152607032e6ba343a0cf0f99a
Greg Tatum <tatum.creative@gmail.com>
2021-01-17 09:44:01 -0600

    Update constants a bit, is this correct?

--- src/constants.rs
+++ src/constants.rs
@@ -5,37 +5,30 @@ pub mod memory_range {
     }
 
     impl Range {
+        #[inline]
         pub fn size(&self) -> u16 {
             self.max - self.min
         }
     }
 
     // 2KB internal RAM
+    pub const RAM_ACTUAL: Range = Range {
+        min: 0x0000,
+        max: 0x07ff,
+    };
+    // The RAM addresses are mirrored a total of 4 times.
     pub const RAM: Range = Range {
         min: 0x0000,
-        max: 0x07FF,
+        max: 0x1fff,
     };
-    // These addresses just look up RAM.
-    pub const RAM_MIRROR_1: Range = Range {
-        min: 0x0800,
-        max: 0x0FFF,
-    };
-    pub const RAM_MIRROR_2: Range = Range {
-        min: 0x1000,
-        max: 0x17FF,
-    };
-    pub const RAM_MIRROR_3: Range = Range {
-        min: 0x1800,
-        max: 0x1FFF,
-    };
-    pub const PPU_REGISTERS: Range = Range {
+    pub const PPU_ACTUAL: Range = Range {
         min: 0x2000,
         max: 0x2007,
     };
     // These mirror the ppu registers every 8 bytes.
-    pub const PPU_REGISTER_MIRRORS: Range = Range {
+    pub const PPU: Range = Range {
         min: 0x2008,
-        max: 0x3FFF,
+        max: 0x4000,
     };
     pub const APU_AND_IO_REGISTERES: Range = Range {
         min: 0x4000,
